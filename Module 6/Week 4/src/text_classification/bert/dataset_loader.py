a52603c9711823e4d512e166f501174e8c462c3e
duyphuongnguyen1999 <duyphuongnguyen1999@gmail.com>
2024-12-31 22:53:30 +0700

    Add dataset_loader.py

---
+++ Module 6/Week 4/src/text_classification/bert/dataset_loader.py
@@ -0,0 +1,33 @@
+from dataset import load_dataset
+from transformers import AutoTokenizer
+from config import MAX_SEQ_LENGTH
+
+model_name = "distilbert-base-uncased"  # bert-based-uncased
+
+tokenizer = AutoTokenizer.from_pretrained(model_name, use_fast=True)
+
+max_seq_length = min(MAX_SEQ_LENGTH, tokenizer.model_max_length)
+
+
+def preprocess_function(examples):
+    # Tokenize the texts
+
+    result = tokenizer(
+        examples["preprocessed_sentence"],
+        padding="max_length",
+        max_length=max_seq_length,
+        truncation=True,
+    )
+    result["label"] = examples["label"]
+
+    return result
+
+
+def load_and_preprocess_dataset(preprocess_function):
+    # Load dataset
+    ds = load_dataset("thainq107/ntc-scv")
+    # Running the preprocessing pipeline
+    processed_dataset = ds.map(
+        preprocess_function, batched=True, desc="Running tokenizer on dataset"
+    )
+    return processed_dataset
