56c42959738c43f34569f6d5630b4d711d152088
duyphuongnguyen1999 <duyphuongnguyen1999@gmail.com>
2025-01-02 01:58:51 +0700

    Add VisionTransformer.py

---
+++ Module 6/Week 4/src/vit_image_classification/model/VisionTransformer.py
@@ -0,0 +1,92 @@
+import torch
+import torch.nn as nn
+
+
+class TransformerEncoder(nn.Module):
+    def __init__(self, embed_dim, num_heads, ff_dim, dropout=0.1):
+        super().__init__()
+        self.attn = nn.MultiheadAttention(
+            embed_dim=embed_dim, num_heads=num_heads, batch_first=True
+        )
+        self.ffn = nn.Sequential(
+            nn.Linear(in_features=embed_dim, out_features=ff_dim, bias=True),
+            nn.ReLU(),
+            nn.Linear(in_features=ff_dim, out_features=embed_dim, bias=True),
+        )
+        self.layernorm_1 = nn.LayerNorm(normalized_shape=embed_dim, eps=1e-6)
+        self.layernorm_2 = nn.LayerNorm(normalized_shape=embed_dim, eps=1e-6)
+        self.dropout_1 = nn.Dropout(p=dropout)
+        self.dropout_2 = nn.Dropout(p=dropout)
+
+    def forward(self, query, key, value):
+        attn_output, _ = self.attn(query, key, value)
+        attn_output = self.dropout_1(attn_output)
+        out_1 = self.layernorm_1(query + attn_output)
+        ffn_output = self.ffn(out_1)
+        ffn_output = self.dropout_2(ffn_output)
+        out_2 = self.layernorm_2(out_1 + ffn_output)
+        return out_2
+
+
+class PatchPositionEmbedding(nn.Module):
+    def __init__(self, image_size=224, embed_dim=512, patch_size=16, device="cpu"):
+        super().__init__()
+        self.conv1 = nn.Conv2d(
+            in_channels=3,
+            out_channels=embed_dim,
+            kernel_size=patch_size,
+            stride=patch_size,
+            bias=False,
+        )
+        scale = embed_dim**-0.5
+        self.positional_embedding = nn.Parameter(
+            scale * torch.randn((image_size // patch_size) ** 2, embed_dim)
+        )
+        self.device = device
+
+    def forward(self, x):
+        x = self.conv1(x)  # shape = [*, width, grid, grid]
+        x = x.reshape(x.shape[0], x.shape[1], -1)  # shape = [*, width, grid**2]
+        x = x.permute(0, 2, 1)  # shape = [*, grid ** 2, width]
+
+        x = x + self.positional_embedding.to(self.device)
+        return x
+
+
+class VisionTransformer(nn.Module):
+    def __init__(
+        self,
+        image_size,
+        embed_dim,
+        num_heads,
+        ff_dim,
+        dropout=0.1,
+        device="cpu",
+        num_classes=10,
+        patch_size=16,
+    ):
+        super().__init__()
+        self.embed_layer = PatchPositionEmbedding(
+            image_size=image_size,
+            embed_dim=embed_dim,
+            patch_size=patch_size,
+            device=device,
+        )
+        self.transformer_layer = TransformerEncoder(
+            embed_dim=embed_dim, num_heads=num_heads, ff_dim=ff_dim, dropout=dropout
+        )
+        # self.pooling = nn.AvgPool1d(kernel_size=max_length)
+        self.fc1 = nn.Linear(in_features=embed_dim, out_features=20)
+        self.fc2 = nn.Linear(in_features=20, out_features=num_classes)
+        self.dropout = nn.Dropout(p=dropout)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        output = self.embed_layer(x)
+        output = self.transformer_layer(output, output, output)
+        output = output[:, 0, :]
+        output = self.dropout(output)
+        output = self.fc1(output)
+        output = self.dropout(output)
+        output = self.fc2(output)
+        return output
